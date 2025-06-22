ddc280c4fb30b87285637e901071bcfb6472566d
sidrk-dev <sidthekoolkid1@gmail.com>
2025-06-21 20:01:03 -0500

    fix issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/nasa-portfolio' : '',
+  assetPrefix: isProd ? '/nasa-portfolio/' : '',
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
